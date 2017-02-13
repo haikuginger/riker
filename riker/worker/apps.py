2fff4600d701d6f5ac9675d96916ca74cf3cfdbd
Jesse Shapiro <jesse@jesseshapiro.net>
2017-02-13 17:48:06 -0500

    Remove prematurely inserted 'ready' method

--- riker/worker/apps.py
+++ riker/worker/apps.py
@@ -7,7 +7,3 @@ from worker.utils import LircListener
 
 class WorkerConfig(AppConfig):
     name = 'worker'
-
-    def ready(self):
-        lirc_name = getattr(settings, 'RIKER_LIRC_LISTENER_NAME', 'riker')
-        LircListener(lirc_name=lirc_name).start()
