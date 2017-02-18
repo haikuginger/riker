581d722a52fda3aa5a22f3f15c752c234363d00a
Jesse Shapiro <jesse@jesseshapiro.net>
2017-02-18 09:04:00 -0500

    Bugfixes all around

--- riker/worker/management/commands/lirc_listen.py
+++ riker/worker/management/commands/lirc_listen.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 
-from worker.utils import listen, createlircrc_tempfile
+from worker.utils import listen, create_lircrc_tempfile
 
 class Command(BaseCommand):
 
